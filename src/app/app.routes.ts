40d7fd2c9fe1af15b2c7056eb005159207c65525
adyelson <adyelson.loureiro@gmail.com>
2025-03-15 18:36:58 -0300

    rotas preparadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { MediaalunosComponent } from './alunos/componentes/mediaalunos/mediaalunos.component';
-import { FormularioComponent } from './alunos/componentes/formulario/formulario.component';
-import { ListagemComponent } from './alunos/componentes/listagem/listagem.component';
 
 export const routes: Routes = [
   {
